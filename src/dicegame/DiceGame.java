e964c381d52614592a9dc39e2da83cc5cc861d10
Andrew Sison <andrewsison19@gmail.com>
2019-10-21 00:21:21 -0700

    Worked on foundation.

--- src/dicegame/DiceGame.java
+++ src/dicegame/DiceGame.java
@@ -10,44 +10,92 @@ package dicegame;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
+import javafx.scene.image.Image;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.scene.layout.BorderPane;
+
+import java.io.FileNotFoundException;
 import java.util.Random;
 
 public class DiceGame extends Application {
 
     final int DICE_AMOUNT = 5;
     BorderPane borderPane;
-    HBox diceLayout;
-    ImageView[] dice; // array of the dice's images
-    boolean[] isHeld; // array of the dice's being held
+    HBox diceLayout; // this will be on the center of the border pane
+
+    // this contains  the button and two labels, score and rollsRemaining
+    VBox bottom; // this will be on the bottom of the border pane
+    Label overallScoreLabel; // this will be on the top side of the border pane, also contains overall score
+    int overallScore = 0;
+    Label roundScoreLabel; // contains the score of the current round
+    int roundScore = 0;
+    Label rollsRemaining;
+    //ImageView[] diceViews; // array of the dice's images
+    Image[] diceImages;
+    Image[] heldImages;
+    Die[] dice;
+    //boolean[] isHeld; // array of the dice's being held
     final int ROLL_MAX = 3;
-    static int rollCount = 0; // roll count is at max, ROLL_MAX
+    static int rollCount = 0; // roll count is at most, ROLL_MAX
     Button button;
+
+    // image settings
+    double imageHeight = 100;
+    double imageWidth = 100;
+    boolean preserveRatio = false;
+    boolean smooth = true;
+
 
     public static void main(String[] args) {
         launch(args);
     }
 
     @Override
-    public void start(Stage primaryStage) {
+    public void start(Stage primaryStage) throws FileNotFoundException {
         borderPane = new BorderPane();
         diceLayout = new HBox();
-        dice = new ImageView[DICE_AMOUNT];
+        overallScoreLabel = new Label("Overall Score: ");
+        roundScoreLabel = new Label("Round Score: ");
+        rollsRemaining = new Label("Rolls Remaining: ");
+        // diceViews = new ImageView[DICE_AMOUNT];
+        diceImages = new Image[6];
+        heldImages = new Image[6];
+        dice = new Die[DICE_AMOUNT];
+
         button = new Button();
 
+        // set the images for the non held dice
+        // index 0 is for the dice value of 1, index 1: value 2, index 2: value 3, etc.
+        diceImages[0] = new Image("file:../../DiceImages/Dice1.png", imageHeight, imageWidth, preserveRatio, smooth);
+        diceImages[1] = new Image("file:../../DiceImages/Dice2.png", imageHeight, imageWidth, preserveRatio, smooth);
+        diceImages[2] = new Image("file:../../DiceImages/Dice3.png", imageHeight, imageWidth, preserveRatio, smooth);
+        diceImages[3] = new Image("file:../../DiceImages/Dice4.png", imageHeight, imageWidth, preserveRatio, smooth);
+        diceImages[4] = new Image("file:../../DiceImages/Dice5.png", imageHeight, imageWidth, preserveRatio, smooth);
+        diceImages[5] = new Image("file:../../DiceImages/Dice6.png", imageHeight, imageWidth, preserveRatio, smooth);
+
+        //set the images for the held dice
+        heldImages[0] = new Image("file:../../DiceImages/Dice1Held.png", imageHeight, imageWidth, preserveRatio, smooth);
+        heldImages[1] = new Image("file:../../DiceImages/Dice2Held.png", imageHeight, imageWidth, preserveRatio, smooth);
+        heldImages[2] = new Image("file:../../DiceImages/Dice3Held.png", imageHeight, imageWidth, preserveRatio, smooth);
+        heldImages[3] = new Image("file:../../DiceImages/Dice4Held.png", imageHeight, imageWidth, preserveRatio, smooth);
+        heldImages[4] = new Image("file:../../DiceImages/Dice5Held.png", imageHeight, imageWidth, preserveRatio, smooth);
+        heldImages[5] = new Image("file:../../DiceImages/Dice6Held.png", imageHeight, imageWidth, preserveRatio, smooth);
+
         // add dice to the HBox, diceLayout.
-        for(int i = 0;i < DICE_AMOUNT; i++) {
-            diceLayout.getChildren().add(dice[i] = new ImageView());
+        for (int i = 0; i < DICE_AMOUNT; i++) {
+            dice[i] = new Die(diceImages,heldImages);
+            diceLayout.getChildren().add(dice[i].getView());
 
             // set the logic for the "Holding" of dice.
             int captureI = i;
-            dice[i].setOnMouseClicked(mouseEvent-> {
-                if(rollCount <= ROLL_MAX) {
-                    //TODO: set image to opposite/held version dice[i].setImage();
-                    isHeld[captureI] = true;
+            dice[i].getView().setOnMouseClicked(mouseEvent -> {
+                if (rollCount <= ROLL_MAX) {
+                    dice[captureI].setHeld(!dice[captureI].getIsHeld());
+                    //isHeld[captureI] = true;
                 }
             });
         }
@@ -58,4 +106,6 @@ public class DiceGame extends Application {
         primaryStage.setScene((new Scene(borderPane)));
         primaryStage.show();
     }
+
+
 }
---
+++ src/dicegame/Die.java
@@ -0,0 +1,80 @@
+package dicegame;
+
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+import java.util.Random;
+
+public class Die {
+    private boolean isHeld;
+    private int value;
+
+    Image[] diceImages;
+    Image[] heldImages;
+    private ImageView view;
+    private Image image;
+
+    public Die(){ }
+
+    public Die(Image[] diceImages, Image[] heldImages) {
+        this.diceImages = diceImages;
+        this.heldImages = heldImages;
+
+        isHeld = false;
+        value = 1;
+        image = diceImages[0]; // TODO add default image?
+        view = new ImageView(image);
+    }
+
+    public void setHeld(boolean held) {
+        isHeld = held;
+        // boolean for whether the held is true or false,
+        // which equates to true or false for either true or false respectively.
+        if(!held ? true : false) {
+            switch (value) {
+                case 1: setImage(diceImages[0]); break;
+                case 2: setImage(diceImages[1]); break;
+                case 3: setImage(diceImages[2]); break;
+                case 4: setImage(diceImages[3]); break;
+                case 5: setImage(diceImages[4]); break;
+                case 6: setImage(diceImages[5]); break;
+                default: System.out.println("Something went wrong at method setHeld of class die, switch 1"); break;
+            }
+        } else {
+            switch (value) {
+                case 1: setImage(heldImages[0]); break;
+                case 2: setImage(heldImages[1]); break;
+                case 3: setImage(heldImages[2]); break;
+                case 4: setImage(heldImages[3]); break;
+                case 5: setImage(heldImages[4]); break;
+                case 6: setImage(heldImages[5]); break;
+                default: System.out.println("Something went wrong at method setHeld of class die, switch 2"); break;
+            }
+        }
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public int roll() {
+        Random rand = new Random();
+        value = rand.nextInt(5) + 1;
+        return value;
+    }
+
+    public ImageView getView(){
+        return view;
+    }
+
+    public boolean getIsHeld(){
+        return isHeld;
+    }
+
+    private void setImage(Image image) {
+        this.image = image;
+        view.setImage(image);
+    }
+
+
+}
